4987279a726cba084b07bd4839563505e93c5206
gregstula <gregstula@me.com>
2016-01-08 02:55:27 -0500

    Restructured directory structure

--- zsh/zsh/oh-my-zsh.sh
+++ zsh/zsh/oh-my-zsh.sh
@@ -1,8 +1,3 @@
-# Check for updates on initial load...
-if [ "$DISABLE_AUTO_UPDATE" != "true" ]; then
-  env ZSH=$ZSH DISABLE_UPDATE_PROMPT=$DISABLE_UPDATE_PROMPT zsh -f $ZSH/tools/check_for_upgrade.sh
-fi
-
 # Initializes Oh My Zsh
 
 # add a function path
