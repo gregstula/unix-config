334fd43909a0516e701176753e35e75baf992837
Gregory D Stula <gregstula.dev@icloud.com>
2017-01-08 01:01:35 -0500

    Add MicrosoftFontInstaller to scripts

---
+++ scripts/MicrosoftFontInstaller.zsh
@@ -0,0 +1,33 @@
+#!/usr/local/bin/zsh
+
+# MicrosoftFontInstaller.zsh
+# The MIT License (MIT) [https://opensource.org/licenses/MIT]
+# Copyright (c) 2016 Gregory D. Stula
+#  
+#  Created by Gregory D. Stula on 1/4/16.
+#  
+
+if [ ! -f /usr/local/bin/cabextract || -f /usr/local/bin/mkfontsdir]; then
+    echo "Dependencies missing! Please run pkg install cabextract mkfontsdir and try again."
+    exit
+fi
+
+set -e
+set -x
+mkdir temp
+cd temp
+fetch http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe
+cabextract -L -F ppviewer.cab PowerPointViewer.exe
+cabextract ppviewer.cab
+
+if [ ! -d ~/fonts ]; then
+    mkdir ~/.fonts
+fi
+
+mv *.TTY ~/.fonts/
+cd ~/.fonts
+mkfontsdir
+xset fp+ ~/.fonts
+xset fp rehash
+
+echo "Successfully installed Microsoft Fonts"
