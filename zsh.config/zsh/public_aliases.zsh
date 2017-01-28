314edb5a780f371b4b7499a6e0a2359cb6b2f550
Gregory D Stula <gregstula.dev@icloud.com>
2017-01-28 02:17:13 -0500

    [zsh] Update FreeBSD rbenv config

--- zsh.config/zsh/public_aliases.zsh
+++ zsh.config/zsh/public_aliases.zsh
@@ -58,6 +58,7 @@ if [ -x /usr/local/bin/rbenv ]; then
     if [[ $KERNAL == "FreeBSD" ]]; then 
   # temporary work around for https://github.com/rbenv/rbenv/issues/881
         export RUBY_CONFIGURE_OPTS=--with-opt-dir=/usr/local
+        export CC=clang
     fi
 fi
 
