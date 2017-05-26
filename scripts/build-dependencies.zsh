a2d3624f2b83cf663e7c0eceffbd3998f9d7123a
Gregory D Stula <gregstula.dev@icloud.com>
2017-05-26 07:14:43 -0400

    [all] Fix build script so it actually works

--- scripts/build-dependencies.zsh
+++ scripts/build-dependencies.zsh
@@ -1,16 +1,16 @@
 #!/bin/env zsh
 
 cd ..
-git submodules update --init --remote
-git submodules update --init --recursive
+git submodule update --init --remote
+git submodule update --init --recursive
 cd vim.config/vim/bundle/YouCompleteMe
 
 # Build YouCompleteMe
 if hash clang 2>/dev/null && hash rustc 2>/dev/null && hash go 2>/dev/null && hash mono 2>/dev/null && hash python 2>/dev/null && hash npm 2>/dev/null; then
-	`./install.py --all`
+	./install.py --all --system-libclang
 elif hash clang 2>/dev/null; then
 	echo "Missing some dependencies for YouCompleteMe, installing with clang-completer only"
-	`./install.py --clang-completer`
+	./install.py --clang-completer
 else
 	echo "Missing clang stack... YouCompleteMe must be built manually after installing atleast clang."
 	echo "Skipping..."
@@ -19,17 +19,17 @@ fi
 # Build zsh-git-status if haskell-stack is installed, otherwise it's not really necessary to function. I.E. we can silently fail here.
 if hash stack 2>/dev/null; then
 	cd ../../../../zsh.config/zsh/plugins/zsh-git-prompt
-	`stack setup`
-	`stack build && stack install`
+	stack setup --allow-different-user
+	stack build --allow-different-user && stack install --allow-different-user
 fi
 
 # Install eclim if eclipse and java are already installed
 if hash java 2>/dev/null && hash curl 2>/dev/null && hash eclipse 2>/dev/null; then
-	cd
-	`curl -O https://github.com/ervandew/eclim/releases/download/2.6.0/eclim_2.6.0.jar`
+	cd ~
+	curl -O https://github.com/ervandew/eclim/releases/download/2.6.0/eclim_2.6.0.jar
 	echo "Running eclim installer..."
-	`java -jar eclim_2.6.0.jar`
-	`rm -rf eclim_2.6.0.jar`
+	java -jar eclim_2.6.0.jar
+	rm -rf eclim_2.6.0.jar
 fi
 
 
