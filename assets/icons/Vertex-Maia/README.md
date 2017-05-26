9eed25b0b657a5f8fa12f7b803587862f8f6afc5
Gregory D Stula <gregstula.dev@icloud.com>
2017-05-26 12:10:50 -0400

    [all] Complete install script
    removed 'assets/'

--- assets/icons/Vertex-Maia/README.md
+++
@@ -1,2 +0,0 @@
-vertex-maia-icon-themes
-=======================
--- scripts/build-dependencies.zsh
+++ scripts/build-dependencies.zsh
@@ -1,33 +1,34 @@
 #!/bin/env zsh
 
-cd ..
-git submodule update --init --remote
-git submodule update --init --recursive
-cd vim.config/vim/bundle/YouCompleteMe
-
-# Build YouCompleteMe
-if hash clang 2>/dev/null && hash rustc 2>/dev/null && hash go 2>/dev/null && hash mono 2>/dev/null && hash python 2>/dev/null && hash npm 2>/dev/null; then
-	./install.py --all --system-libclang
-elif hash clang 2>/dev/null; then
-	echo "Missing some dependencies for YouCompleteMe, installing with clang-completer only"
-	./install.py --clang-completer
-else
-	echo "Missing clang stack... YouCompleteMe must be built manually after installing atleast clang."
-	echo "Skipping..."
-fi
-
-# Build zsh-git-status if haskell-stack is installed, otherwise it's not really necessary to function. I.E. we can silently fail here.
-if hash stack 2>/dev/null; then
-	cd ../../../../zsh.config/zsh/plugins/zsh-git-prompt
-	stack setup --allow-different-user
-	stack build --allow-different-user && stack install --allow-different-user
-fi
+# cd ..
+# git submodule update --init --remote
+# git submodule update --init --recursive
+# cd vim.config/vim/bundle/YouCompleteMe
+#
+# # Build YouCompleteMe
+# if hash clang 2>/dev/null && hash rustc 2>/dev/null && hash go 2>/dev/null && hash mono 2>/dev/null && hash python 2>/dev/null && hash npm 2>/dev/null; then
+# 	./install.py --all --system-libclang
+# elif hash clang 2>/dev/null; then
+# 	echo "Missing some dependencies for YouCompleteMe, installing with clang-completer only"
+# 	./install.py --clang-completer
+# else
+# 	echo "Missing clang stack... YouCompleteMe must be built manually after installing atleast clang."
+# 	echo "Skipping..."
+# fi
+#
+# # Build zsh-git-status if haskell-stack is installed, otherwise it's not really necessary to function. I.E. we can silently fail here.
+# if hash stack 2>/dev/null; then
+# 	cd ../../../../zsh.config/zsh/plugins/zsh-git-prompt
+# 	stack setup --allow-different-user
+# 	stack build --allow-different-user && stack install --allow-different-user
+# fi
 
 # Install eclim if eclipse and java are already installed
 if hash java 2>/dev/null && hash curl 2>/dev/null && hash eclipse 2>/dev/null; then
-	cd ../../../../vendor
 	echo "Running eclim installer..."
-	java -jar eclim.jar
+	JAR_PATH=`git rev-parse --show-toplevel`
+	JAR_PATH=$JAR_PATH/vendor/eclim.jar
+	java -jar $JAR_PATH
 fi
 
 
