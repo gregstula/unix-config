62cea395230d32ba2c21b25ef591d89f9256b84d
Greg Stula <gregstula@me.com>
2024-07-21 03:30:49 -0500

    Remove outdated files

--- nvim.config/nvim/init.lua
+++ nvim.config/nvim/init.lua
@@ -3,3 +3,25 @@ vim.cmd.source(vimrc)
 
 -- Package manager
 require("config.lazy")
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Make line numbers default
+vim.opt.number = true
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
