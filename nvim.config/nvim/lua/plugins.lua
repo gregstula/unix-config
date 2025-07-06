37dcc239e63224665fb0a9303145cd8e744e9626
Greg Stula <gregstula@me.com>
2025-07-05 22:05:35 -0500

    Update vimrcs and zshrcs

--- nvim.config/nvim/lua/plugins.lua
+++ nvim.config/nvim/lua/plugins.lua
@@ -344,7 +344,7 @@ return {
                 -- clangd = {},
                 -- gopls = {},
                 -- pyright = {},
-                -- rust_analyzer = {},
+                rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
                 -- Some languages (like typescript) have entire language plugins that can be useful:
