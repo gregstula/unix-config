d32c4cb1b9997c57b6bbfa9ce25a65eafc24b706
Greg Stula <gregstula@me.com>
2025-07-24 19:33:21 -0500

    Add package manager and tree-sitter to nvim

---
+++ konsole/Minty.profile
@@ -0,0 +1,22 @@
+[Appearance]
+ColorScheme=minty
+Font=Consolas,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
+LineSpacing=1
+WordMode=false
+
+[Cursor Options]
+CursorShape=1
+
+[General]
+Name=Minty
+Parent=FALLBACK/
+TerminalMargin=1
+
+[Scrolling]
+HighlightScrolledLines=false
+HistoryMode=2
+SearchLineOpacity=90
+
+[Terminal Features]
+BidiRenderingEnabled=true
+BlinkingCursorEnabled=true
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,50 +1,29 @@
 -- vimrc always overrides on session reload
 vim.opt.sessionoptions:remove("options")
 
--- vim not vi
-vim.opt.compatible = false
-
-vim.cmd('filetype plugin indent on')
-
-vim.cmd('syntax on')
 -- vim.opt.number = true
 
--- Auto indent
-vim.opt.ai = true
--- Smart indent
-vim.opt.si = true
-
 -- Treats tabs like buffers ... questionable
-vim.cmd('tab sball')
-
--- Allow backspacing over autoindent, line breaks and start of insert action
-vim.opt.backspace = "indent,eol,start"
-
--- This shows what you are typing as a command.
-vim.opt.showcmd = true
+vim.cmd("tab sball")
 
 -- Visualize tabs
 -- vim.cmd('syntax match Tab /\\t/')
-vim.cmd('hi Tab gui=underline guifg=blue ctermbg=blue')
+vim.cmd("hi Tab gui=underline guifg=blue ctermbg=blue")
 
 -- globally use spaces instead of tabs
 vim.opt.expandtab = true
 vim.opt.ts = 4
 vim.opt.sw = 4
 
--- Be smart when using tabs ;)
-vim.opt.smarttab = true
-
 -- default character width 100
 vim.opt.tw = 120
 vim.opt.colorcolumn = "120"
 vim.opt.cursorline = true
-vim.opt.ruler = true
 
 -- Autotrim trailing whitespace on exit
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    command = "%s/\\s\\+$//e"
+	pattern = "*",
+	command = "%s/\\s\\+$//e",
 })
 vim.opt.wrap = true
 vim.opt.linebreak = true
@@ -53,18 +32,26 @@ vim.opt.showbreak = "> \\ \\ "
 -- Hide the mouse pointer while typing
 vim.opt.mousehide = true
 
--- Make the command-line completion better
-vim.opt.wildmenu = true
-
 -- Zee greatest shell
 vim.opt.shell = "zsh"
 
+-- KEYMAPS --
+-- For the meme
+vim.keymap.set("n", "<Up>", "<nop>")
+vim.keymap.set("n", "<Down>", "<nop>")
+vim.keymap.set("n", "<Left>", "<nop>")
+vim.keymap.set("n", "<Right>", "<nop>")
+
 -- With a map leader it's possible to do extra key combinations
 -- like <leader>w saves the current file
-vim.g.mapleader = ",,"
-vim.g.maplocalleader = ",,"
--- Pressing ,ss will toggle and untoggle spell checking
-vim.keymap.set('n', '<leader>ss', ':setlocal spell!<cr>')
+-- Default leader is \ which is what I use
+
+-- A lot of neovim users use space or comma as the leader
+-- vim.g.mapleader = ","
+-- vim.g.maplocalleader = " "
+
+-- Pressing \ss will toggle and untoggle spell checking
+vim.keymap.set("n", "<leader>ss", ":setlocal spell!<cr>")
 
 -- Yank to clipboard
 vim.opt.clipboard = "unnamedplus"
@@ -73,17 +60,42 @@ vim.opt.clipboard = "unnamedplus"
 -- vim.cmd('color molokai')
 -- Actually I use this one now (I made it myself)
 -- vim.cmd('color stula')
-vim.g.airline_theme = 'base16_monokai'
 
 -- Transparent
 -- vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
 
--- Unicode
-vim.opt.encoding = "utf-8"
+-- Plugins
+-- TODO: Replace this with the native built-in neovim package manager that was released July 2025
+-- Eventually we want a guanteed to be here and working plugin config
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
 
--- For the meme
-vim.keymap.set('n', '<Up>', '<nop>')
-vim.keymap.set('n', '<Down>', '<nop>')
-vim.keymap.set('n', '<Left>', '<nop>')
-vim.keymap.set('n', '<Right>', '<nop>')
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
+-- Setup lazy.nvim
+require("lazy").setup({
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
+		build = ":TSUpdate",
+	},
+})
