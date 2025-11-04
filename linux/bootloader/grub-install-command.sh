9fc48451974814f71d02361ec1c9dcaaa4f19fcc
Gregory Stula <gregstula@users.noreply.github.com>
2025-11-03 19:39:39 -0600

    Update grub script

--- linux/bootloader/grub-install-command.sh
+++ linux/bootloader/grub-install-command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ $(id -u) -ne 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
     echo "Need sudo!"
-    exit -1
+    exit 1
 fi
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB --modules="tpm btrfs lvm cryptodisk luks" --disable-shim-lock
 grub-mkconfig -o /boot/grub/grub.cfg
